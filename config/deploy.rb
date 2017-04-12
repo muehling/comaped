4536e2d5932a90f175bf0d67b8cc89c1f5371f2b
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2017-04-12 17:33:54 +0200

    YAML

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, ' amuehling@levumi.informatik.uni-kiel.de:/var/git/comaped.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+  set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
