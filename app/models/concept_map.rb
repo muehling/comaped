7227c7bae50b72afd069a53b77b38fe962024ca0
morten <stu114712@yahoo.de>
2018-05-02 12:17:38 +0200

    bugfix legende?

--- app/models/concept_map.rb
+++ app/models/concept_map.rb
@@ -21,7 +21,8 @@ class ConceptMap < ApplicationRecord
   #        Otherwise, if a start map is given, the necessary concepts and links are created.
   #Returns: -
   def after_create
-    #self.data["legend"] ={"rgb(223, 240, 216)"=> "1","rgb(173,255,47)"=> "2","rgb(0,128,0)"=> "3","rgb(221,160,221)"=> "4","rgb(255,255,0)"=> "5","rgb(255,165,0)"=> "6","rgb(30,144,255)"=> "7", "rgb(0,191,255)"=> "8"}
+    self.data["legend"] ={"rgb(223, 240, 216)"=> " ","rgb(173,255,47)"=> " ","rgb(0,128,0)"=> " ","rgb(221,160,221)"=> " ","rgb(255,255,0)"=> " ","rgb(255,165,0)"=> " ","rgb(30,144,255)"=> " ", "rgb(0,191,255)"=> " "}
+
     self.accesses ||= 0
     unless survey.concept_labels.blank?
       labels = survey.concept_labels.split(',').map{|s| s.strip}.uniq
