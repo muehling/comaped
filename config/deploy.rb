c9f97286ab254cf5197e90458c4f92a529f8fe3d
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2019-02-08 15:22:30 +0100

    Bundler update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.1'
+lock '3.11.0'
 
 set :application, 'comaped'
 set :repo_url, ' amuehling@levumi.informatik.uni-kiel.de:/var/git/comaped.git'
