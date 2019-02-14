63dc9a1a11254caa11d677333fbaea9bc6133683
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2019-02-15 00:29:17 +0100

    Canvas size

--- app/helpers/concepts_helper.rb
+++ app/helpers/concepts_helper.rb
@@ -8,14 +8,18 @@ module ConceptsHelper
     ['#cfe0c8', '#0ea5c6', '#ffcdab', '#becbff', '#ded473', '#ff8484']
   end
 
+  def default_color
+    return colors[0]
+  end
+
   def palette(i)
-    return colors[i] || '#dff0d8'
+    return colors[i]
   end
 
   def get_highlight(color)
     i = colors.index(color)
     if i.nil?
-      return '#cfe0c8'
+      return default_color
     else
       return highlights[colors.index(color)]
     end
