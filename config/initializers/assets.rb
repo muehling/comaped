a825f598aec1826975b74253584e7286c36ffd83
Zafarelli <jens@pardulon-models.com>
2022-06-28 13:19:48 +0200

    Upgrade Rails to V7

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css.sass, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( timeline/* network/* jquery.simplecolorpicker.*)
+Rails.application.config.assets.precompile += %w( timeline/* network/* jquery.simplecolorpicker.* )
+Rails.application.config.assets.precompile += %w( ConceptMap.js)
+Rails.application.config.assets.precompile += %w( entrypoint.js application.js Intro.js)
