2cd194cb1cd31601c651127ca330f08230dabd81
Jens Beckmann <jbe@informatik.uni-kiel.de>
2022-08-09 09:19:04 +0200

    Introduce vite bundler in favor of import maps

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+import FullReload from 'vite-plugin-full-reload'
+
+
+export default defineConfig({
+  plugins: [
+    RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 })
+  ],
+  server: {
+    host: 'localhost',
+    port: 3036,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+    }
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': 'bootstrap',
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ['@import "./app/frontend/scss/application";']
+      }
+    }
+  },
+})
