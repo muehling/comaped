31f7679e063e0e37f2d5d9f5984c84a314481b70
Jens Beckmann <jbe@informatik.uni-kiel.de>
2022-10-14 07:10:13 +0200

    Remove unnecessary switch from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,28 @@
 Rails.application.routes.default_url_options[ENV['RAILS_RELATIVE_URL_ROOT']]
 Rails.application.routes.draw do
-  routes_config =
-    Proc.new do
-      resources :concept_maps, path: 'maps', param: :code, only: %i[edit update show] do
-        resources :concepts
-        resources :links
-        resources :versions
-      end
+  resources :concept_maps, path: 'maps', param: :code, only: %i[edit update show] do
+    resources :concepts
+    resources :links
+    resources :versions
+  end
 
-      scope 'backend' do
-        root 'application#backend'
-        resources :users do
-          post '/projects/import', to: 'projects#import'
-          resources :projects do
-            resources :surveys do
-              get 'concept_maps/page', to: 'concept_maps#page'
-              resources :concept_maps, param: :code, except: %i[edit update]
-            end
-          end
+  scope 'backend' do
+    root 'application#backend'
+    resources :users do
+      post '/projects/import', to: 'projects#import'
+      resources :projects do
+        resources :surveys do
+          get 'concept_maps/page', to: 'concept_maps#page'
+          resources :concept_maps, param: :code, except: %i[edit update]
         end
       end
-
-      post 'login' => 'application#login'
-      get 'logout' => 'application#logout'
-      post 'send_code' => 'application#send_code'
-      get 'map/(:code)' => 'application#map_link'
-      post 'map' => 'application#map_form'
-      root 'application#frontend', as: :frontend
     end
-
-  if ENV['RAILS_RELATIVE_URL_ROOT']
-    scope(path: ENV['RAILS_RELATIVE_URL_ROOT']) { routes_config.call }
-  else
-    routes_config.call
   end
+
+  post 'login' => 'application#login'
+  get 'logout' => 'application#logout'
+  post 'send_code' => 'application#send_code'
+  get 'map/(:code)' => 'application#map_link'
+  post 'map' => 'application#map_form'
+  root 'application#frontend', as: :frontend
 end
