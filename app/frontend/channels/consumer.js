2e6aefd5e0b5e18c1cadd92c9ccbcd3dd369ffc3
Jens Beckmann <jbe@informatik.uni-kiel.de>
2022-10-19 16:35:48 +0200

    Add WS url for channel

--- app/frontend/channels/consumer.js
+++ app/frontend/channels/consumer.js
@@ -1,6 +1,9 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
 
-import { createConsumer } from "@rails/actioncable"
+import { createConsumer } from '@rails/actioncable'
 
-export default createConsumer()
+// für das aktuelle Staging-System:
+export default createConsumer('wss://ruapehu.informatik.uni-kiel.de/comaped/cable')
+
+//export default createConsumer()
